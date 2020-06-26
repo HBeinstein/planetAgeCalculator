9550a18270dee047fc1ab15f580bc5b68ca63f0c
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-26 11:05:12 -0700

    add updated passing test for instantiating new User object

--- src/user.js
+++ src/user.js
@@ -1,8 +1,8 @@
 export class User {
   constructor(name, dateOfBirth, ageInEarthDays) {
-    // this.name = name;
-    // this.dateOfBirth = dateOfBirth;
-    // this.ageInEarthDays = ageInEarthDays
+    this.name = name;
+    this.dateOfBirth = dateOfBirth;
+    this.ageInEarthDays = ageInEarthDays
   }
 
   checkAgeMars() {
